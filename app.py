672f709d0ed0293ee13f930cf9531d11eabcbeeb
Tuan07minh <tuannguzen@gmail.com>
2026-01-19 10:37:30 +0700

    cap nhat

--- app.py
+++ app.py
@@ -2318,9 +2318,7 @@ def health_check():
     }), 200
 
 # Hoặc đơn giản hơn:
-@app.route('/ping')
-def ping():
-    return "pong", 200
+
 ###
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
